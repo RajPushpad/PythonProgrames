51c66ab904853d99f7da5f3690e054f28ae39e92
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 15:06:30 +0530

    Create 22. calculate the power using ‘pow()’ method.py
    Add of calculate the power using 'POW( )'method

---
+++ 22. calculate the power using ‘pow()’ method.py
@@ -0,0 +1,6 @@
+# Program to calculate the power using ‘pow()’ method
+
+base = int(input("Enter the value for base :"))
+exponent = int(input("Enter the value for exponent :"))
+print(base,"to  the power ",exponent,"=",end = ' ')
+print(pow(base,exponent))
