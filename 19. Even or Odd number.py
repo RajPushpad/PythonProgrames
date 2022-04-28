c92425f750772d8e386e3179437d94905ea60c68
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 14:46:45 +0530

    Create 19. Even or Odd number.py
    Add Even number or Odd Number

---
+++ 19. Even or Odd number.py
@@ -0,0 +1,8 @@
+# Program Check a given number is even or odd
+
+num = int(input("Enter a number to check even/odd "))  
+
+if num%2 == 0:
+   print(num,"is even number")
+else:
+   print(num,"is odd number")
