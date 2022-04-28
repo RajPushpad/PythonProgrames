2a70ed52f2f50528eba2dd4aa924461ac1f473af
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 14:26:45 +0530

    Create 14. Add two integer without using arithmetic operator.py
    Add two integer Without using arithmetic operator

---
+++ 14. Add two integer without using arithmetic operator.py
@@ -0,0 +1,6 @@
+# Program of Add two integer without using arithmetic operator
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+p=num1-(-num2)
+print("Sum of two numbers is",p)
