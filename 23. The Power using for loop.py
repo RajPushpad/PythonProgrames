e6f6be0f6784be7a57027b7f3b444bfb85f15997
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 15:09:32 +0530

    Create 23. The Power using for loop.py
    Add The power using for loop

---
+++ 23. The Power using for loop.py
@@ -0,0 +1,10 @@
+# program to calculate the power using ‘for-loop’
+
+base = int(input("Enter the value for base :"))
+exponent = int(input("Enter the value for exponent :"))
+result=1;
+print(base,"to power ",exponent,"=",end = ' ')
+#using ‘for’ loop with ‘range’ function
+for exponent in range(exponent, 0, -1):
+    result *= base
+print(result)
