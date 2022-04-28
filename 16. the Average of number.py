fb5a316d2262dbee4aa8c2360702b555b7f5d6c9
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 14:33:59 +0530

    Create 16. the Average of number.py
    Add of the Average of number

---
+++ 16. the Average of number.py
@@ -0,0 +1,10 @@
+# Program of the Average of number with explanations  --> 16
+
+def findAverage(avg,avg2):
+    
+    return (avg+avg2)/2
+
+avg= int(input( 'Please enter a number: '))
+avg2 = int(input('Please enter a number: '))
+
+print('The average of {first} and {second} numbers: ', findAverage(avg, avg2))
