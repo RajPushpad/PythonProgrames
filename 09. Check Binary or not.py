06b0d287dea545c769a2f674ae6e29ba24e6981a
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 13:51:21 +0530

    Create 09. Check Binary or not.py
    Add of Binary number or not

---
+++ 09. Check Binary or not.py
@@ -0,0 +1,13 @@
+# Program of Check  a number is binary
+
+num = int(input("please give a number : "))
+while(num>0):
+    j=num%10
+    if j!=0 and j!=1:
+        print("num is not binary")
+        break
+    num=num//10
+    if num==0:
+        print("num is binary") 
+
+
