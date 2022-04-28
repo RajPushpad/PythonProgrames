289b9dbfd5a26a2a53cb1cc287ccc17a62187740
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 14:15:22 +0530

    Create 13. Prime factor of given integer.py
    Add of Prime factor of given integer

---
+++ 13. Prime factor of given integer.py
@@ -0,0 +1,7 @@
+# Program of Prime factor of a given integer
+           
+def print_factors(x):
+       print("The factors of",x,"are:")
+       for i in range(1, x + 1):
+         if x % i == 0:
+           print(i)
