6502687bfeadd4c44ce27da861c373a1edace725
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-28 14:11:39 +0530

    Create 12. Swapping of two number usig third variable.py
    Swapping of two Number using third variable

---
+++ 12. Swapping of two number usig third variable.py
@@ -0,0 +1,11 @@
+# Program of Swapping of two numbers using third variable
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number: "))
+num3 = num1
+num1 = num2
+num2 = num3
+print('Swaping number')
+print('value num1 is :',num1)
+print('value of num2 is : ',num2)
+
