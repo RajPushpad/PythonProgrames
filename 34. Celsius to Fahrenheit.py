59d8d1b66aa3717df3bfc41bbdf7bc10994d3b78
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-29 13:51:39 +0530

    Create 34. Celsius to Fahrenheit.py
    Add to Convert Celsius to Fahrenheit

---
+++ 34. Celsius to Fahrenheit.py
@@ -0,0 +1,7 @@
+# Program to Convert Celsius to Fahrenheit
+
+print("Enter Temperature in Fahrenheit: ")
+fahren = float(input())
+celi = (fahren-32)/1.8
+
+print("\nEquivalent Temperature in Celsius: ", celi)
