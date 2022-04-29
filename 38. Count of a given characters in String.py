6a3594c806427f82c02a44a80695cc032a2564cc
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-29 15:02:31 +0530

    Create 38. Count of a given characters in String.py
    Add to count occurrence of a given Characters in string

---
+++ 38. Count of a given characters in String.py
@@ -0,0 +1,7 @@
+# program to count occurrence of a given characters in string
+
+
+str = input("Enter a string : ")
+ch = input("Enter a char.: ")     
+p = str.count(ch)              
+print('Total Number of occurence of',p)
