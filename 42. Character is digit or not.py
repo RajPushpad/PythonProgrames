e8cbe7dcf9925e0ff40dde45f3bfc015585fb672
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-29 15:25:18 +0530

    Create 42. Character is digit or not.py
    Add to Check given Character is digit or not

---
+++ 42. Character is digit or not.py
@@ -0,0 +1,8 @@
+# Program to check given character is digit or not
+
+
+ch = input("Enter a character : ") 
+if ch >= '0' and ch <= '9':  
+    	print("Given Character ", ch, "is a Digit")
+else:
+    print("Given Character ", ch, "is not a Digit")
