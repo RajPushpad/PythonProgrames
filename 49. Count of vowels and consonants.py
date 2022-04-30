309f711e471877363333e59022d0950bd19244bc
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-30 15:05:20 +0530

    Create 49. Count of vowels and consonants.py
    Add the Program to count occurrences of vowels and consonants in a string

---
+++ 49. Count of vowels and consonants.py
@@ -0,0 +1,13 @@
+# program to count occurrences of vowels and consonants in a string 
+
+
+
+string = input("Enter a String : ")   
+vowels = 0  
+consonants = 0
+for i in string:   
+    if i in ('a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U'):  
+        vowels+=1 
+    elif i.isalpha():  
+        consonants+=1  
+print("Vowels :",vowels,"Consonants:",consonants)
