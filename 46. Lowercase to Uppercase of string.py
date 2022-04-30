bd28a8a6ffa5e4a821ec8864660a9ee5ce64f9d0
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-30 14:15:21 +0530

    Create 46. Lowercase to Uppercase of string.py
    Add to convert lowercase char to uppercase of string

---
+++ 46. Lowercase to Uppercase of string.py
@@ -0,0 +1,9 @@
+# program to convert lowercase char to uppercase of string
+
+string = input("Enter a String : ")
+result=''
+for i in string:  
+        if i.islower():  
+            i = i.upper() 
+        result += i 
+print("String after converting lower case to upper :",result)
