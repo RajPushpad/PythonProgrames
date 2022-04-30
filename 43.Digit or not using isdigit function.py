56108789cb01b9599d558113f2b1161a59968645
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-30 13:50:37 +0530

    Create 43.Digit or not using isdigit function.py
    Add to Check Given Character is digit or not using 'isdigit( )' function

---
+++ 43.Digit or not using isdigit function.py
@@ -0,0 +1,11 @@
+# Program to check given character is digit or not using ‘isdigit()’ function
+
+ch = input("Enter a character : ") 
+# checks if character ‘ch’ is digit or not
+if(ch.isdigit()): 
+    print("The Given Character ", ch, "is a Digit")
+else:
+    print("The Given Character ", ch, "is not a Digit")
+
+
+
