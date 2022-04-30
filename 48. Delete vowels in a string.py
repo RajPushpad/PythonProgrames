17e4cb75966a27ee82bd006c9e1e50c2a7fea315
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-30 14:50:30 +0530

    Create 48. Delete vowels in a string.py
    Add the Program to delete vowels in a given string

---
+++ 48. Delete vowels in a string.py
@@ -0,0 +1,9 @@
+# Program to delete vowels in a given string
+
+string = input("Enter a String : ") 
+result=''
+for i in string:  
+    if i in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):  
+        i = ''
+    result += i
+print("String after removing the vowels :",result)
