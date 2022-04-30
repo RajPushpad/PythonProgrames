3d00e786c4dd29277eac13db63c747df502c4c46
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-04-30 14:05:59 +0530

    Create 45. Replace straing  using replace() function.py
    Add to the replace String Space with Character using replace( ) function

---
+++ 45. Replace straing  using replace() function.py
@@ -0,0 +1,5 @@
+# Program to the replace string space with  given character using replace() function
+
+str = input("Enter a String : ") 
+ch = input("Enter a Character : ")
+print("String after removing space with",ch,"=",str.replace(" ",ch))
