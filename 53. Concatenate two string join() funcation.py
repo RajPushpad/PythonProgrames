83dff78f8f2200b51a9ee98bc77f298192ee154c
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-05-01 14:26:17 +0530

    Create 53. Concatenate two string join() funcation.py
    Add Program to concatenate two string using join( ) funcation

---
+++ 53. Concatenate two string join() funcation.py
@@ -0,0 +1,6 @@
+# Program to concatenate two string using join() function
+
+str1 = input('Enter first string: ')
+str2 = input('Enter second string: ')
+
+print("String after concatenation :","".join([str1, str2]))
