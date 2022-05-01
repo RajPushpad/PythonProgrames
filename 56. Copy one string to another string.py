bc208d31ff819878b472d147f5d04f57e63cfc71
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-05-01 15:17:07 +0530

    Create 56. Copy one string to another string.py
    Add the program to copy one string to another string

---
+++ 56. Copy one string to another string.py
@@ -0,0 +1,9 @@
+# program to copy one string to another string
+
+str1 = input("Enter The String: ")
+
+str2 = str1
+str3 = str1[:]
+
+print("The Final String : Str2  = ", str2)
+print("The Final String : Str3  = ", str3)
