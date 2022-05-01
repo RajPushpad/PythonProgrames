b43589c86b1ec3540a342445d8bbbbf55b74e79d
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-05-01 14:11:33 +0530

    Create 51. Replace first occurrence of vowel.py
    Add program to Replace first occurrence of vowel with '-' i n string

---
+++ 51. Replace first occurrence of vowel.py
@@ -0,0 +1,13 @@
+# Program to Replace First Occurrence of vowel with '-'in string
+
+string = input("Please enter your own String : ")
+char = input("Please enter your own Character : ")
+string2 = ''
+length = len(string)
+for i in range(length):
+    if(string[i] == char):
+        string2 = string[0:i] + string[i + 1:length]
+        break
+ 
+print("Original String :  ", string)
+print("Final String :     ", string2)
