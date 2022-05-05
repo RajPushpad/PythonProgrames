75ec0754ebe2d1224b1a123e6469667b76460fc8
Raj Pushpad <104488262+RajPushpad@users.noreply.github.com>
2022-05-05 14:21:14 +0530

    Create 68. Insert element at end of array.py
    Add the program to insert an element at end of array

---
+++ 68. Insert element at end of array.py
@@ -0,0 +1,6 @@
+# Program to insert an element at end of array 
+
+arr = [1,2,3,4,5]
+num=int(input("Enter a number to insert in array at end :"))
+arr.append(num)
+print("Array after inserting",num,"at end",arr)
